74dd3ee0402fc170c417941a526068975a090af5
Adithya S <84130203+Adithya2441@users.noreply.github.com>
2022-06-16 16:20:00 +0530

    Upload to local folder

--- fms/fms/settings.py
+++ fms/fms/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = '/media/'
--- fms/fmsapp/urls.py
+++ fms/fmsapp/urls.py
@@ -1,9 +1,14 @@
 from django import views
+from django.conf import settings
 from django.urls import path
 from django.contrib import admin
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.Home.as_view(),name='home'),
     path('upload/',views.upload,name='upload'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
